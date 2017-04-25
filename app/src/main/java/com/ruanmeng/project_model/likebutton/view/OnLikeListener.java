394d954faac7b7951245dce0c07f78bc21e9519a
Administrator <iamlifuchang@163.com>
2017-04-25 08:53:43 +0800

    更改

--- app/src/main/java/com/ruanmeng/project_model/likebutton/view/OnLikeListener.java
+++
@@ -1,9 +0,0 @@
-package com.ruanmeng.project_model.likebutton.view;
-
-/**
- * Created by Joel on 23/12/2015.
- */
-public interface OnLikeListener {
-    void liked(LikeButton likeButton);
-    void unLiked(LikeButton likeButton);
-}
--- app/src/main/java/com/ruanmeng/project_model/myselectview/text.java
+++
@@ -1,8 +0,0 @@
-package com.ruanmeng.project_model.myselectview;
-
-/**
- * Created by Administrator on 2016/10/17.
- */
-public class text
-{
-}
